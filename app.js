ab0b4312de460e4416bafde47bb0f30e9cbee1b5
Rishav Raj <rishav.24b1541080@abes.ac.in>
2026-05-30 12:17:08 +0530

    Issue Fixes + db Addition

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+require("dotenv").config();
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo').default;
+const User = require('./models/user');
+
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("db Connected")
+    })
+    .catch((err) => {
+        console.log("Failed", err);
+    })
+
+
 const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    resetToken: String,
+    resetTokenExpiration: Date,
+})
